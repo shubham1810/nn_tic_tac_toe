c43d7046b4a221582f11bdf856391338db80504a
Shubham Dokania <skd.1810@gmail.com>
2015-10-26 16:34:22 +0530

    declare initial neural network class

---
+++ neural_net.py
@@ -0,0 +1,23 @@
+"""
+Neural Network Class to save data about the net and train the player
+"""
+import numpy as np
+import random
+
+
+class NeuralNet:
+
+    def __init__(self):
+        self.INPUTS = 9
+        self.LAYERS = 1
+        self.OUTPUTS = 1
+
+        # create a weight matrix
+        self.weights = []
+
+        for i in range(self.LAYERS):
+            pass
+
+
+if __name__ == '__main__':
+    print np
