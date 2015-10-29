1c868dc47a0b94f20b25b8102b72b3d28ea8e32f
Shubham Dokania <skd.1810@gmail.com>
2015-10-29 15:07:03 +0530

    Modify NN to include classification matrix way

--- range_tests.py
+++ range_tests.py
@@ -5,7 +5,7 @@ import random
 nx = 9
 n1 = 10
 n2 = 10
-ny = 1
+ny = 9
 
 # w1 = np.zeros((nx, n1))
 # w2 = np.zeros((n2, n1)).T
@@ -16,58 +16,56 @@ N = 100
 min_val = 10000
 max_val = -10000
 ll = []
-while ix < N:
-    ix += 1
-    w1 = np.random.rand(nx, n1)
-    w2 = np.random.rand(n2, n1)
-    w3 = np.random.rand(n2, 1)
 
-    # x = np.zeros((nx, 1))
-    # l1 = np.zeros((1, n1))
-    # l2 = np.zeros((1, n2))
+ix += 1
+w1 = np.random.rand(nx, n1)
+w2 = np.random.rand(n2, n1)
+w3 = np.random.rand(n2, ny)
 
-    inp = [0, 0, 1, 1, -1, -1, 0, 0, 0]
-    # x = np.random.rand(nx, 1)
-    x = np.asarray(inp)
-    x = np.reshape(x, (1, len(inp)))
+# x = np.zeros((nx, 1))
+# l1 = np.zeros((1, n1))
+# l2 = np.zeros((1, n2))
 
+inp = [-1, 1, 1,
+       1, -1, -1,
+       1, -1, 1]
 
-    # l1 = np.random.rand(1, n1)
-    # l2 = np.random.rand(1, n2)
+activation = []
+for i in inp:
+    if i == 0:
+        activation.append(1)
+    else:
+        activation.append(0)
+print activation
 
+# x = np.random.rand(nx, 1)
+x = np.asarray(activation)
+x = np.reshape(x, (1, len(inp)))
+print x.shape
 
-    # print w1
-    # print w2
-    # print w3
-    # print x
-    # print l1
-    # print l2
-
-    def sigmoid(x):
-        return 5.0 / (1 + np.exp(-1 * x))
-
-
-    l1 = np.dot(x, w1)
-    # l1 = sigmoid(l1)
-    l1 = np.tanh(l1) + random.random()
-    l2 = np.dot(l1, w2.T)
-    # l2 = sigmoid(l2)
-    l2 = np.tanh(l2) + random.random()
-    y = np.dot(l2, w3)
-    # y = sigmoid(y)
-    # y = np.tanh(y)
-    y_val = abs(np.round(9 * y / 15))
-    sum_val += y_val
-    ll.append(y_val)
-    if y_val < min_val:
-        min_val = y_val
-
-    if y_val > max_val:
-        max_val = y_val
+# print w1
+# print w2
+# print w3
+# print x
+# print l1
+# print l2
 
 
-# print np.round(sum_val/N)
-print min_val, max_val
+def sigmoid(z):
+    return 5.0 / (1 + np.exp(-1 * z))
 
-for i in range(10):
-    print i, "::::", ll.count(i)
+
+l1 = np.dot(x, w1)
+# l1 = sigmoid(l1)
+l1 = np.tanh(l1)
+l2 = np.dot(l1, w2.T)
+# l2 = sigmoid(l2)
+l2 = np.tanh(l2)
+y = np.dot(l2, w3)
+y = (np.exp(y))/(np.sum(np.exp(y)))  # softmax
+y_out = []
+for ix in range(y.shape[1]):
+    y_out.append(y[0][ix] * activation[ix])
+print y_out
+print np.argmax(y_out)
+
